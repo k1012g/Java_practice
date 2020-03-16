7e8cceada137e76c9aba005b70dbdbf7a2fa1528
k1012g <kuri.ladybaby1012@gmail.com>
2020-03-17 00:06:50 +0900

    chapter06 done

---
+++ chapter06/Kouhan.java
@@ -0,0 +1,15 @@
+package comment;
+
+public class Kouhan {
+	public static void callDeae() {
+		System.out.println("ええい、小癪な。曲者だ！出会えい！");
+	}
+
+	public static void showMondokoro() throws InterruptedException {
+		System.out.println("飛車さん、角さん。もういいでしょう。");
+		System.out.println("この紋所が目に入らぬか！");
+		Thread.sleep(3000);
+
+		Zenhan.doTogame();
+	}
+}
