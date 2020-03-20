a2ee6defdd24a804cf5671d1452e49350d86f0a9
k1012g <kuri.ladybaby1012@gmail.com>
2020-03-20 13:55:12 +0900

    chapter15 done

---
+++ chapter15/Hero.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Hero {
+	private String name;
+
+	public Hero(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public static void main(String[] args) {
+		Hero h1 = new Hero("斎藤");
+		Hero h2 = new Hero("鈴木");
+
+		Map<Hero, Integer> heroes = new HashMap<Hero, Integer>();
+		heroes.put(h1, 3);
+		heroes.put(h2, 7);
+
+		for (Hero key : heroes.keySet()) {
+			int value = heroes.get(key);
+			System.out.println(key.getName() + "が倒した敵=" + value);
+		}
+	}
+}
---
+++ chapter15/List.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class List {
+	public static void main(String[] args) {
+		ArrayList<Integer> points = new ArrayList<Integer>();
+
+		points.add(10);
+		points.add(80);
+		points.add(75);
+
+		for (int i : points) {
+			System.out.println(i);
+		}
+	}
+}
