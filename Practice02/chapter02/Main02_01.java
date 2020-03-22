054fb5a612f1ac4ecc4bf0425e820b8c2dba7382
k1012g <kuri.ladybaby1012@gmail.com>
2020-03-22 14:54:13 +0900

    chapter02_01 done

---
+++ Practice02/chapter02/Main02_01.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main02_01 {
+	public static void main(String[] args) {
+		Date now = new Date();
+		System.out.println(now);
+		System.out.println(now.getTime());
+		Date past = new Date(1316622225935L);
+		System.out.println(past);
+	}
+}
