173cde73ec3636044288223eab557e8dc4eedd96
k1012g <kuri.ladybaby1012@gmail.com>
2020-04-09 14:44:30 +0900

    review03 done

---
+++ Review01/Main03.java
@@ -0,0 +1,13 @@
+public class Main03 {
+	public static void main(String[] args) {
+		int[] moneyList = {122902, 8302, 55100};
+
+		// for (int i = 0; i < moneyList.length; i++) {
+		// 	System.out.println(moneyList[i]);
+		// }
+
+		for (int m : moneyList) {
+			System.out.println(m);
+		}
+	}
+}
