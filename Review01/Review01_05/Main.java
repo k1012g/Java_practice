c6b47ef6c413b30ee74f69c2ce9f38f8c4307be6
k1012g <kuri.ladybaby1012@gmail.com>
2020-04-11 16:28:18 +0900

    review05 done

---
+++ Review01/Review01_05/Main.java
@@ -0,0 +1,11 @@
+import comment.Zenhan;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		comment.Kouhan k1 = new comment.Kouhan();
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		k1.callDeae();
+		comment.Kouhan.showMondokoro();
+	}
+}
